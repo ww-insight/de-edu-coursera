1c7be9ca095e7767210e729fbaa330a96f82900b
wwbel <ww.bel@ya.ru>
2023-01-09 14:00:10 +0300

    final assignment

--- 8-etl-airflow-kafka/finalassignment/ETL_toll_data.py
+++ 8-etl-airflow-kafka/finalassignment/ETL_toll_data.py
@@ -7,6 +7,8 @@ from airflow.operators.bash_operator import BashOperator
 # This makes scheduling easy
 from airflow.utils.dates import days_ago
 
+bash_cwd = '/opt/airflow/dags/finalassignment/'
+
 # defining DAG arguments
 # You can override them on a per-task basis during operator initialization
 default_args = {
@@ -32,6 +34,7 @@ dag = DAG(
 unzip_data = BashOperator(
     task_id='unzip_data',
     bash_command='tar -zxf tolldata.tgz',
+    cwd=bash_cwd,
     dag=dag,
 )
 
@@ -39,24 +42,28 @@ unzip_data = BashOperator(
 extract_data_from_csv = BashOperator(
     task_id='extract_data_from_csv',
     bash_command='cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
+    cwd=bash_cwd,
     dag=dag,
 )
 
 extract_data_from_tsv = BashOperator(
     task_id='extract_data_from_tsv',
-    bash_command='cut -d$''\\t'' -f5-7 tollplaza-data.tsv | tr $''\\t'' '','' | tr -d $''\\r'' > tsv_data.csv',
+    bash_command='cut -d$\'\\t\' -f5-7 tollplaza-data.tsv | tr $\\\t\' \',\' | tr -d $\'\\r\' > tsv_data.csv',
+    cwd=bash_cwd,
     dag=dag,
 )
 
 extract_data_from_fixed_width = BashOperator(
     task_id='extract_data_from_fixed_width',
-    bash_command='cut -c 59- payment-data.txt | tr '' '' '','' > fixed_width_data.csv',
+    bash_command='cut -c 59- payment-data.txt | tr " " "," > fixed_width_data.csv',
+    cwd=bash_cwd,
     dag=dag,
 )
 
 consolidate_data = BashOperator(
     task_id='consolidate_data',
-    bash_command='paste -d '','' csv_data.csv tsv_data.csv fixed_width_data.csv >  extracted_data.csv',
+    bash_command='paste -d \',\' csv_data.csv tsv_data.csv fixed_width_data.csv >  extracted_data.csv',
+    cwd=bash_cwd,
     dag=dag,
 )
 
@@ -64,7 +71,8 @@ consolidate_data = BashOperator(
 
 transform_data = BashOperator(
     task_id='transform_data',
-    bash_command='awk -F'','' ''{ print $1","$2","$3","toupper($4)","$5","$6","$7","$8","$9; }'' < extracted_data.csv > staging/transformed_data.csv',
+    bash_command='awk -F\',\' \'{ print $1","$2","$3","toupper($4)","$5","$6","$7","$8","$9; }\' < extracted_data.csv > staging/transformed_data.csv',
+    cwd=bash_cwd,
     dag=dag,
 )
 
