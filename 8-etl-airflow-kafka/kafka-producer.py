f517f1f02d516273c13bb75c2eef28d22fe0b304
wwbel <ww.bel@ya.ru>
2023-01-09 10:09:55 +0300

    producer upd

--- 8-etl-airflow-kafka/kafka-producer.py
+++ 8-etl-airflow-kafka/kafka-producer.py
@@ -1,7 +1,7 @@
-from kafka import KafkaConsumer, KafkaAdminClient
+from kafka import KafkaAdminClient, KafkaProducer
+import json
 
 admin_client = KafkaAdminClient(bootstrap_servers="192.168.0.101:9092", client_id='test')
-consumer = KafkaConsumer('bankbranch')
-for msg in consumer:
-    print(msg.value.decode("utf-8"))
-
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer.send("bankbranch", {'atmid':1, 'transid':100})
+producer.send("bankbranch", {'atmid':2, 'transid':101})
--- 8-etl-airflow-kafka/kafka-topics.py
+++ 8-etl-airflow-kafka/kafka-topics.py
@@ -4,7 +4,7 @@ from kafka.admin import NewTopic, ConfigResource, ConfigResourceType
 admin_client = KafkaAdminClient(api_version=(2, 8, 0), bootstrap_servers="192.168.0.101:9092", client_id='test')
 topic_list = []
 
-new_topic = NewTopic(name="bankbranch2", num_partitions= 2, replication_factor=1)
+new_topic = NewTopic(name="bankbranch", num_partitions= 2, replication_factor=1)
 topic_list.append(new_topic)
 admin_client.create_topics(new_topics=topic_list)
 
