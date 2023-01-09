d235d08e207334c811ff5986a9196748c69413bd
wwbel <ww.bel@ya.ru>
2023-01-09 18:01:06 +0300

    final assignment kafka

---
+++ 8-etl-airflow-kafka/fin-kafka/kafka-mysql.sql
@@ -0,0 +1,11 @@
+create database tolldata;
+use tolldata;
+
+create table livetolldata(
+  timestamp datetime,
+  vehicle_id int,
+  vehicle_type char(15),
+  toll_plaza_id smallint
+);
+
+select * from livetolldata limit 10;
